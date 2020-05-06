175e4e15cc6769c53a19ab0acbe684df9d9af0e0
braiscaloto <braiscaloto92@gmail.com>
2020-05-06 14:04:04 +0200

    handlers

--- main.go
+++ main.go
@@ -1,5 +1,19 @@
 package main
 
+import (
+	"log"
+
+	"github.com/braiscaloto/Twittor/bd"
+	"github.com/braiscaloto/Twittor/handlers"
+)
+
 func main() {
+
+	if bd.CheckConnectionDB() == 0 {
+		log.Fatal("No DB connection")
+		return
+	}
+
+	handlers.Handlers()
 
 }
