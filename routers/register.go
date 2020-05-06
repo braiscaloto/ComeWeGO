1d785b43a4a4bf514ce6d384cc2625678bec8a98
braiscaloto <braiscaloto92@gmail.com>
2020-05-06 16:40:53 +0200

    conflicts

--- routers/register.go
+++ routers/register.go
@@ -2,10 +2,14 @@ package register
 
 import (
 	"net/http"
+
+	"github.com/braiscaloto/Twittor-backend/models"
 )
 
 /*Register es la función para crear el registro de usuarios en la base de datos*/
 func Register(w http.ResponseWriter, r *http.Request) {
+
+	var t models.User
 
 	/*err := json.NewDecoder(r.Body).Decode(&t)
 
